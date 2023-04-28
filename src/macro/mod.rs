1fd1f9964774f88c739598b4bf775547cab33db4
wangfeng <wangfeng@jyinform.com>
2023-04-28 14:52:02 +0800

    1

--- src/macro/mod.rs
+++ src/macro/mod.rs
@@ -1,16 +1,19 @@
-use crate::r#macro::scripts::get_scripts;
+use crate::{hid, r#macro::scripts::get_scripts};
 use defmt::*;
 use defmt_rtt as _;
 
 mod scripts;
 
-pub fn run(script: &str, delay: &mut cortex_m::delay::Delay) {
+pub fn run(script: &str, delay: &mut cortex_m::delay::Delay) -> ! {
     info!("script: {}", script);
-    let vec = get_scripts();
-    for v in vec {
-        info!("{}", v.name());
-    }
+    // let vec = get_scripts();
+    // for v in vec {
+    //     info!("{}", v.name());
+    // }
     loop {
         delay.delay_ms(1);
+        hid::pro_controller::set_input_line("A");
+        delay.delay_ms(1);
+        hid::pro_controller::set_input_line("");
     }
 }
