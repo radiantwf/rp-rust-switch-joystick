8db0a01d26fa6c72f692543cf51c1b6a50b40ff7
radiantwf <radiantwf@gmail.com>
2023-07-04 10:54:45 +0800

    1

--- src/uart.rs
+++ src/uart.rs
@@ -1,3 +1,4 @@
+use core::fmt::Write;
 use cortex_m::singleton;
 use cortex_m::{delay::Delay, prelude::_embedded_hal_serial_Read};
 use hal::{
@@ -8,11 +9,13 @@ use hal::{
     Timer,
 };
 use rp2040_hal as hal;
-// a20201010000000000a303
+// a201010000000000a3
+
 use crate::hid;
 
-const STX: [u8; 2] = [0xA2, 0x02];
-const ETX: [u8; 2] = [0xA3, 0x03];
+const STX: &[u8] = b"\xA2";
+const ETX: &[u8] = b"\xA3";
+const BUFFER_LENGTH: usize = 7;
 
 pub fn run(
     _uart: UartPeripheral<
@@ -27,14 +30,17 @@ pub fn run(
     _timer: Timer,
     _delay: Delay,
 ) -> ! {
-    let (_rx, _tx) = _uart.split();
+    let (_rx, mut _tx) = _uart.split();
     _tx.write_full_blocking(b"UART Connected\r\n");
-    let rx_buf = singleton!(: [u8; 11] = [0; 11]).unwrap();
+    let rx_buf =
+        singleton!(: [u8; BUFFER_LENGTH + STX.len() + ETX.len()] = [0; BUFFER_LENGTH + STX.len() + ETX.len()]).unwrap();
     let mut rx_transfer = hal::dma::single_buffer::Config::new(_dma.ch0, _rx, rx_buf).start();
     loop {
         let (ch0, mut rx, rx_buf) = rx_transfer.wait();
 
         let len_rx_buf = rx_buf.len();
+        writeln!(_tx, "received {} bytes", len_rx_buf).unwrap();
+
         let mut misalignment = false;
         for (i, v) in STX.iter().enumerate() {
             if rx_buf[i] != *v {
@@ -43,12 +49,13 @@ pub fn run(
             }
         }
         for (i, v) in ETX.iter().enumerate() {
-            if rx_buf[len_rx_buf - 2 + i] != *v {
+            if rx_buf[len_rx_buf - ETX.len() + i] != *v {
                 misalignment = true;
                 break;
             }
         }
         if misalignment {
+            _tx.write_full_blocking(b"data misalignment\r\n");
             let mut _etx_index = 0;
             loop {
                 let _result = rx.read();
@@ -56,9 +63,6 @@ pub fn run(
                     Ok(v) => {
                         if v == ETX[_etx_index] {
                             _etx_index += 1;
-                            if _etx_index == ETX.len() {
-                                break;
-                            }
                         } else {
                             _etx_index = 0;
                         }
@@ -67,13 +71,16 @@ pub fn run(
                         _etx_index = 0;
                     }
                 }
+                if _etx_index == ETX.len() {
+                    _tx.write_full_blocking(b"ok\r\n");
+                    break;
+                }
             }
         } else {
-            let mut buf = [0u8; 7];
-            buf.clone_from_slice(&rx_buf[2..9]);
+            let mut buf = [0u8; BUFFER_LENGTH];
+            buf.clone_from_slice(&rx_buf[STX.len()..len_rx_buf - ETX.len()]);
             hid::pro_controller::set_input_uart_buffer(buf);
         }
-
         rx_transfer = hal::dma::single_buffer::Config::new(ch0, rx, rx_buf).start();
     }
 }
